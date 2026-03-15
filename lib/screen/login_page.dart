48349d8d578bf3b1efddd29a5b045d288e2dcd2f
gogopowercoding <furaihanalharits@gmail.com>
2026-03-15 20:16:44 +0700

    fixx

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -12,6 +12,7 @@ class _LoginpageState extends State<Loginpage> {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passController = TextEditingController();
   bool isLogin = false;
+  bool isPasswordHidden = true;
 
   Map<String, String> users = {
     "furaihan": "108",
@@ -59,18 +60,29 @@ class _LoginpageState extends State<Loginpage> {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+
+              const Icon(
+                Icons.calculate,
+                size: 90,
+                color: Colors.blue,
+              ),
+
+              const SizedBox(height: 10),
               const Text(
-                          "Login",
-                          style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                        ),
-              const Text(
-                          "Oleh 086_092_097_108",
-                        ),
+                "Login",
+                style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+
+              const Text("Oleh 086_092_097_108"),
+
+              const SizedBox(height: 20),
+
               usernameField(usernameController),
               passField(passController),
               buttonLogin(),
@@ -83,37 +95,59 @@ class _LoginpageState extends State<Loginpage> {
 
   Widget usernameField(TextEditingController username) {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.symmetric(vertical: 10),
       child: TextField(
         controller: username,
         decoration: InputDecoration(
-          hintText: "username",
-          contentPadding: EdgeInsets.all(8),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+          hintText: "Username",
+          prefixIcon: const Icon(Icons.person),
+          contentPadding: const EdgeInsets.all(8),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
         ),
       ),
     );
   }
 
-  Widget passField(TextEditingController pass) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 20),
-      child: TextField(
-        controller: pass,
-        obscureText: true,
-        decoration: InputDecoration(
-          hintText: "Password",
-          contentPadding: EdgeInsets.all(8),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+Widget passField(TextEditingController pass) {
+  return Container(
+    padding: const EdgeInsets.symmetric(vertical: 10),
+    child: TextField(
+      controller: pass,
+      obscureText: isPasswordHidden,
+      decoration: InputDecoration(
+        hintText: "Password",
+        prefixIcon: const Icon(Icons.lock),
+        suffixIcon: IconButton(
+          icon: Icon(
+            isPasswordHidden
+                ? Icons.visibility_off
+                : Icons.visibility,
+          ),
+          onPressed: () {
+            setState(() {
+              isPasswordHidden = !isPasswordHidden;
+            });
+          },
+        ),
+
+        contentPadding: const EdgeInsets.all(8),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
         ),
       ),
-    );
-  }
+    ),
+  );
+}
 
   Widget buttonLogin() {
     return SizedBox(
       width: double.infinity,
-      child: ElevatedButton(onPressed: login, child: Text("Login")),
+      child: ElevatedButton(
+        onPressed: login,
+        child: const Text("Login"),
+      ),
     );
   }
 }
